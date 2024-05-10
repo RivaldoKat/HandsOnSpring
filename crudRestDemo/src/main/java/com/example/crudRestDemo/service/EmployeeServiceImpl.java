18b3114a2be951b98154c380fc343d2272fcb49a
RivaldoKat <Rivaldokat25@gmail.com>
2024-05-10 10:58:27 +0200

    using JpaRepository for DAO

--- crudRestDemo/src/main/java/com/example/crudRestDemo/service/EmployeeServiceImpl.java
+++ crudRestDemo/src/main/java/com/example/crudRestDemo/service/EmployeeServiceImpl.java
@@ -1,42 +1,53 @@
 package com.example.crudRestDemo.service;
 
-import com.example.crudRestDemo.dao.EmployeeDAO;
+
+import com.example.crudRestDemo.dao.EmployeeRepository;
 import com.example.crudRestDemo.entity.Employee;
-import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int id) {
-        return employeeDAO.findById(id);
+
+        Optional<Employee> results = employeeRepository.findById(id);
+
+        Employee employee = null;
+        if (results.isPresent()) {
+            employee = results.get();
+        }
+        else {
+            throw new RuntimeException("ID not found - " + results);
+        }
+
+        return employee;
     }
 
-    @Transactional
     @Override
     public Employee save(Employee employee) {
-        return employeeDAO.save(employee);
+        return employeeRepository.save(employee);
     }
 
-    @Transactional
     @Override
     public void deleteById(int id) {
-        employeeDAO.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
