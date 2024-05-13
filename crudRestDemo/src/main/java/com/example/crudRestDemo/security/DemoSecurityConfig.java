bdfb7b9aa5f35e1fa152bafe745af90662d0a5fd
RivaldoKat <Rivaldokat25@gmail.com>
2024-05-13 16:10:42 +0200

    introducing bcrypt encryption

--- crudRestDemo/src/main/java/com/example/crudRestDemo/security/DemoSecurityConfig.java
+++ crudRestDemo/src/main/java/com/example/crudRestDemo/security/DemoSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -40,7 +41,7 @@ public class DemoSecurityConfig {
         http.httpBasic(Customizer.withDefaults());
 
         // disable Cross Site Request Forgery (CSRF)
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
