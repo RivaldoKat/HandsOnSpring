3ceea6a5fa84a6295e2e0fa217c00e1f911ffcd9
RivaldoKat <Rivaldokat25@gmail.com>
2024-05-13 13:37:50 +0200

    jdbc auth - plaintext

--- crudRestDemo/src/main/java/com/example/crudRestDemo/security/DemoSecurityConfig.java
+++ crudRestDemo/src/main/java/com/example/crudRestDemo/security/DemoSecurityConfig.java
@@ -5,36 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+   // add support for jdbc ... no more hardcoded users
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public UserDetailsManager userDetailsManager(DataSource datasource) {
 
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}testing")
-                .roles("EMPLOYEE")
-                .build();
+        return new JdbcUserDetailsManager(datasource);
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}testing")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails suzan = User.builder()
-                .username("suzan")
-                .password("{noop}testing")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, suzan, mary);
     }
 
     @Bean
